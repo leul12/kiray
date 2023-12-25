22f91c35181e4f39c31fe81faae0318c4a17adab
leul12 <derejeleul911@gmail.com>
2023-12-25 16:07:32 +0300

    nav_bar_done

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
-import { IconType } from "react-icons"
+import { IconType } from "react-icons";
+import qs from "query-string"
 interface CAtegoryBoxProps{
     icon:IconType;
     label:string;
@@ -16,13 +17,30 @@ interface CAtegoryBoxProps{
     const router = useRouter();
     const params= useSearchParams();
     const handleClick = useCallback(()=>{
-
-    },[]);
+        let currentQuery = {};
+        if(params){
+            currentQuery = qs.parse(params.toString());
+        }
+        const updatedQuery: any ={
+            ...currentQuery,
+            category: label
+        }
+        if(params?.get('category') == label){
+            delete updatedQuery.category;
+        }
+        const url = qs.stringifyUrl({
+            url:'/',
+            query: updatedQuery
+        },{skipNull:true})
+        router.push(url);
+    },[label,params,router]);
     return (
-        <div className={`flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer
+        
+        <div onClick={handleClick} className={`flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer
         ${selected ? 'border-b-neutral-800':'border-transparent'} 
         ${selected ? 'text-neutral-800':'text-neutral-500'}
-        `}>
+        `}
+        >
 <Icon size={26}/>
             <div className="font-medium text-sm">
                 {label}
--- app/components/navbar/Catagories.tsx
+++ app/components/navbar/Catagories.tsx
@@ -1,9 +1,11 @@
 'use client'
 import Container from "../Container"
-import {TbBeach} from 'react-icons/tb'
-import {GiWindmill} from 'react-icons/gi'
+import {TbBeach, TbMountain, TbPool} from 'react-icons/tb'
+import {IoDiamond} from 'react-icons/io5';
+import {GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiDesert, GiForestCamp, GiIsland, GiJungle, GiWindmill} from 'react-icons/gi'
 import CategoryBox from '../CategoryBox'
 import {MdOutlineVilla} from'react-icons/md'
+import { usePathname, useSearchParams } from "next/navigation"
 export const categories = [
     {
         label: 'Beach',
@@ -20,9 +22,73 @@ export const categories = [
         icon:MdOutlineVilla,
         description: 'This Property is Modern!'
     },
-
+    {
+        label: 'Country Side',
+        icon:GiJungle,
+        description: 'This Property isin the country side!'
+    },
+    {
+        label: 'Pools',
+        icon:TbPool,
+        description: 'This Property Has a Pool !'
+    },
+    {
+        label: 'Islands',
+        icon:GiIsland,
+        description: 'This Property is surrounded by water body !'
+    },
+    {
+        label: 'Lake',
+        icon:GiBoatFishing,
+        description: 'This property is near a lake !'
+    },
+    {
+        label: 'Mountain Top',
+        icon:TbMountain,
+        description: 'This Property is at mountain Tops !'
+    },
+    {
+        label: 'Castles',
+        icon:GiCastle,
+        description: 'This Property has castles !'
+    },
+    {
+        label: 'Camping',
+        icon:GiForestCamp,
+        description: 'This Property has camping activities !'
+    },
+    {
+        label: 'Desert',
+        icon:GiCactus,
+        description: 'This Property is in a desert !'
+    },
+    
+    {
+        label: 'cave',
+        icon:GiCaveEntrance,
+        description: 'This Property is in a caves !'
+    },
+    {
+        label: 'Barns',
+        icon:GiBarn,
+        description: 'This Property is in barn !'
+    },
+    {
+        label: 'Lux',
+        icon:IoDiamond,
+        description: 'This Property is luxurious !'
+    },
+    
 ]
 const Catagories=  () =>{
+    const params = useSearchParams();
+    const category = params?.get('category');
+    const pathname = usePathname();
+    const isMainPage = pathname == '/';
+    if(!isMainPage){
+        return null;
+    }
+
     return(
         <Container>
         <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
@@ -30,8 +96,9 @@ const Catagories=  () =>{
             <CategoryBox
               key={item.label}
               label={item.label}
-              description={item.description}
+              selected={category == item.label}
               icon={item.icon}
+              description=""
             />
           ))}
         </div>
